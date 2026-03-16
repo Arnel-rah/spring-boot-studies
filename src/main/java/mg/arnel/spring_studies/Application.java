0c17f81ed3dac4c591e7a55604f5193debeb482b
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-16 20:11:45 +0300

    chore: remove manual bean calls from Application main

--- src/main/java/mg/arnel/spring_studies/Application.java
+++ src/main/java/mg/arnel/spring_studies/Application.java
@@ -2,19 +2,12 @@ package mg.arnel.spring_studies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(Application.class, args);
-
-		OrderService orderService = context.getBean(OrderService.class);
-
-		orderService.findAll();
-		orderService.findById(1);
-		orderService.findByStatus("PENDING");
-		orderService.findOrderItems(1);
+		SpringApplication.run(Application.class, args);
 	}
+
 }
