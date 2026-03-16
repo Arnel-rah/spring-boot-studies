6aae6b8192064a5aaf02599f1814abe1e73f94d1
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-16 20:10:59 +0300

    feat: add Order and OrderItem model classes

--- src/main/java/mg/arnel/spring_studies/Order.java
+++ src/main/java/mg/arnel/spring_studies/Order.java
@@ -1,4 +1,20 @@
 package mg.arnel.spring_studies;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.security.Timestamp;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Order {
+    private int id;
+    private String client;
+    private double total;
+    private String status;
+    private Timestamp createdAt;
+
 }
