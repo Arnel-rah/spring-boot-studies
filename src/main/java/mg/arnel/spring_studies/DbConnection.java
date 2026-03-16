6007e73bd66b2f44051b4dca4ce74850b852e922
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-16 19:44:07 +0300

    feat: create DbConnection component using DriverManager

--- src/main/java/mg/arnel/spring_studies/DbConnection.java
+++ src/main/java/mg/arnel/spring_studies/DbConnection.java
@@ -1,4 +1,29 @@
 package mg.arnel.spring_studies;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
 public class DbConnection {
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver PostgreSQL introuvable", e);
+        }
+    }
 }
